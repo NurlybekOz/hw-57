2b34dd89d84b629ee3616f60c4c77f247fcd1e17
nurlybek <orozovnurlybek@gmail.com>
2025-01-17 15:52:24 +0600

    Импортированы все компоненты, создан метод добавления пользователей

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import './App.css'
+import {useState} from "react";
+import {User} from "./types";
+import UserForm from "./components/UserForm/UserForm.tsx";
+import Users from './components/Users/Users.tsx';
+
 
 const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
 
+  const addUser = (newUser: User) => {
+    setUsers(prevState => [...prevState, newUser]);
+  }
   return (
-    <>
+      <>
+        <div className='container'>
+          <div className="row mt-3">
+            <div className='col-6'>
+              <UserForm onSubmitForm={addUser}/>
+            </div>
+            <div className='col-6'>
+                <Users users={users}></Users>
+            </div>
+          </div>
 
-    </>
+
+        </div>
+      </>
   )
 };
 
